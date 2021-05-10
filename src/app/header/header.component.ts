d75c0a14a55b336faf5284b213dbd8739a413f29
Darien-Cepani <darien.cepani42@gmail.com>
2021-05-10 19:13:43 +0200

    Starting redesign

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-header',
@@ -6,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  oldPrice : number = 9000;
+  newPrice : number = 4500;
+  currTime: number = -1;
+  timerCookieName: string = "firstAccessTime";
 
-  constructor() { }
 
+  constructor( private cookieService: CookieService ) {
+    var time = new Date();
+    var hours = time.getHours()*3600;
+    var minutes = time.getMinutes()*60;
+    var seconds = time.getSeconds();
+    var timeNow = hours + minutes + seconds;
+
+    if(!cookieService.check(this.timerCookieName)){
+      this.currTime = timeNow;
+
+      this.cookieService.set( this.timerCookieName, this.currTime.toString() );
+    } else{
+      let timerCookieValue = this.cookieService.get(this.timerCookieName);
+      if(timerCookieValue != undefined){
+        var cookieTime = parseInt(timerCookieValue);
+        this.currTime = cookieTime + (timeNow - cookieTime);
+      }
+    }
+  }
   ngOnInit(): void {
   }
 
--- src/app/order-form/order-form.component.ts
+++ src/app/order-form/order-form.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-order-form',
@@ -9,26 +8,8 @@ import { CookieService } from 'ngx-cookie-service';
 export class OrderFormComponent implements OnInit {
   oldPrice : number = 9000;
   newPrice : number = 4500;
-  currTime: number = -1;
-  timerCookieName: string = "firstAccessTime";
 
-  constructor( private cookieService: CookieService ) {
-
-    if(!cookieService.check(this.timerCookieName)){
-      var time = new Date();
-      var hours = time.getHours()*3600;
-      var minutes = time.getMinutes()*60;
-      var seconds = time.getSeconds();
-      this.currTime = hours + minutes + seconds;
-
-      this.cookieService.set( this.timerCookieName, this.currTime.toString() );
-    } else{
-      let timerCookieValue = this.cookieService.get(this.timerCookieName);
-      if(timerCookieValue != undefined){
-        this.currTime = parseInt(timerCookieValue);
-      }
-    }
-  }
+  constructor() {}
 
   ngOnInit(): void {
   }
