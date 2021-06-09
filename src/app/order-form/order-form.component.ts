ea42f260059b7a65c197f9ad61afe6cad92a0d11
Darien-Cepani <darien.cepani42@gmail.com>
2021-06-10 00:20:53 +0200

    Bitrix connection

--- src/app/order-form/order-form.component.ts
+++ src/app/order-form/order-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, Inject, Input, PLATFORM_ID, ViewChild } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-order-form',
@@ -9,7 +10,36 @@ export class OrderFormComponent implements OnInit {
   oldPrice : number = 9000;
   newPrice : number = 4500;
 
-  constructor() {}
+  @ViewChild('script') script!: ElementRef;
+  src: any;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+  }
+
+  convertToScript() {
+      console.log(this);
+      const element = this.script.nativeElement;
+      const parent = element.parentElement;
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = 'assets/bitrix.js';
+      script.setAttribute('data-b24-form', 'nline/6/lf913i');
+      script.setAttribute('data-skip-moving', 'true');
+      console.log('Karinpidh');
+      console.log(script);
+
+      if (element.innerHTML) {
+          script.innerHTML = element.innerHTML;
+      }
+
+      parent.parentElement.replaceChild(script, parent);
+  }
+
+  ngAfterViewInit() {
+      if (isPlatformBrowser(this.platformId)) {
+          this.convertToScript();
+      }
+  }
 
   ngOnInit(): void {
   }
---
+++ src/assets/bitrix.js
@@ -0,0 +1,4 @@
+(function(w,d,u){
+  var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/180000|0);
+  var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+})(window,document,'https://cdn.bitrix24.com/b17835355/crm/form/loader_6.js');
